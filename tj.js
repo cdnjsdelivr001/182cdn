e68401437ed83c4043e0676ea1db96642d902cac
cdnjsdelivr001 <44692566+cdnjsdelivr001@users.noreply.github.com>
2018-11-02 15:36:50 +0800

    Create tj.js

---
+++ tj.js
@@ -0,0 +1,12 @@
+//document.body.innerHTML=document.body.innerHTML.replace(/pic.221zx/g,'pic.99thingz');
+var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4074942,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();
+
+document.write ('<script language="javascript" type="text/javascript" src="/js/notice.js"></script>');
