790c4211df32e0cd7fc19f2590931fa648be5e15
cdnjsdelivr001 <44692566+cdnjsdelivr001@users.noreply.github.com>
2018-11-02 15:19:10 +0800

    Create menu2.js

---
+++ menu2.js
@@ -0,0 +1,24 @@
+var topmenu=' <table style="width:100%;text-align: center;">'+
+'<tbody><tr>'+
+'<td style="padding:2px 0px"><a href="/Html/60/">国产精品</a></td>'+
+'<td><a href="/Html/61/">S级女优</a></td>'+
+'<td><a href="/Html/62/">欧美性爱</a></td>'+
+'<td><a href="/Html/109/">经典三级</a></td>'+
+'<td><a href="/Html/101/">成人动漫</a></td>'+
+'</tr>'+
+'<tr>'+
+'<td style="padding:2px 0px"><a href="/Html/89/">自拍偷拍</a></td>'+
+'<td><a href="/Html/90/">换妻游戏</a></td>'+
+'<td><a href="/Html/110/">无码在线</a></td>'+
+'<td><a href="/Html/91/">网红主播</a></td>'+
+'<td><a href="/Html/111/">熟女人妻</a></td>'+
+'</tr>'+
+'<tr>'+
+'<td style="padding:2px 0px"><a href="/Html/112/">美颜巨乳</a></td>'+
+'<td><a href="/Html/113/">颜射吃精</a></td>'+
+'<td><a href="https://www.48vc.com/main.html?m=reg" target="_blank" style="color:#FFA000"><b>开元棋牌</b></a></td>'+
+'<td><a href="https://x32p.com/main.html?m=reg" target="_blank" style="color:#FFA000"><b>皇冠体育</b></a></td>'+
+'<td><a href="https://dd461.com/main.html?m=reg" target="_blank" style="color:#FFA000"><b>金沙赌场</b></a></td>'+
+'</tr>'+
+'</tbody></table>';
+document.writeln(topmenu);
